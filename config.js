dbbc8682c6770a999f894f570f853efd86b091ad
David Tennander <david.tennander@gmail.com>
2023-09-04 21:11:46 +0200

    Update config.js
    Lägger till cool låt!

--- config.js
+++ config.js
@@ -13,5 +13,10 @@ window.songs = [
         feeling: "melankolisk slutscen",
         offset_ms: 0,
         uri: "spotify:track:0fjQ4u99SVRZFUILPs8yM1"
+    },
+    {
+        feeling: "Kul!!!",
+        offset_ms: 10_000,
+        uri: "spotify:track:0ac0IMOQCG2Xt5H9Ja70pU"
     }
 ]
