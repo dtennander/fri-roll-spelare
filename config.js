64d5b6c1f19bdf9db6da203921b7bb825dbdf11f
trollhummer <144378968+trollhummer@users.noreply.github.com>
2023-09-07 21:00:46 +0200

    Update config.js
    härjar in massa låtar! /sally

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ window.songs = [
     {
         feeling: "sad",
         offset_ms: 0,
-        uri: "spotify:track:0fjQ4u99SVRZFUILPs8yM1"
+        uri: "spotify:track:6zh0jASo2jjuPrM0mTMCIh"
     },
     {
         feeling: "melankolisk slutscen",
@@ -18,5 +18,78 @@ window.songs = [
         feeling: "Kul!!!",
         offset_ms: 10_000,
         uri: "spotify:track:0ac0IMOQCG2Xt5H9Ja70pU"
+    },
+        {
+        feeling: "Dramatisk melankoli",
+        offset_ms: 10_000,
+        uri: "spotify:track:4PRnYCH5hoxypKVcOBvx3a"
+    },
+    {
+        feeling: "Dystert bråk",
+        offset_ms: 0,
+        uri: "spotify:track:4HE2Ex0bjbj3YNXmV01OoM"
+    },
+        {
+        feeling: "Lyckligt slut",
+        offset_ms: 0,
+        uri: "spotify:track:73JnItJ3dofEssv5hWFHDs"
+    },
+            {
+        feeling: "Mellanlåt Höghuset",
+        offset_ms: 0,
+        uri: "spotify:track:4pVE63OGS5BkXl04trGVug"
+    },
+    
+            {
+        feeling: "Konstig stämning",
+        offset_ms: 0,
+        uri: "spotify:track:27j3JCRnPvr0QS5LC4zQMn"
+    },
+        
+            {
+        feeling: "Undertrycka känslor",
+        offset_ms: 0,
+        uri: "spotify:track:5ZqRgvkhpdImSoKvutrQHm"
+    },
+    
+            {
+        feeling: "Falling in love",
+        offset_ms: 0,
+        uri: "spotify:track:1OX2lkGZmC0uHPNHAxbFln"
+    },
+      {
+        feeling: "Andligt",
+        offset_ms: 0,
+        uri: "spotify:track:09uiagFAlQyUuzL2h4sA3p"
+    },
+          {
+        feeling: "Nu lämnar jag den här skiten",
+        offset_ms: 0,
+        uri: "spotify:track:0puKNGpsvyFpWoWF4PLFMU"
+    },
+              {
+        feeling: "Kärlek Kärlek Kärlek",
+        offset_ms: 0,
+        uri: "spotify:track:254bXAqt3zP6P50BdQvEsq"
+    },
+                  {
+        feeling: "PARTY",
+        offset_ms: 0,
+        uri: "spotify:track:36RTcbIM1CAOcUT8IZVCKy"
+    },
+                      {
+        feeling: "Hej jag gillar dig",
+        offset_ms: 0,
+        uri: "spotify:track:3sUWj8s6HBXGu20GXZ0QlE"
+    },
+                          {
+        feeling: "Hej jag gillar dig",
+        offset_ms: 0,
+        uri: "spotify:track:3sUWj8s6HBXGu20GXZ0QlE"
+    },
+                              {
+        feeling: "Mysig glad",
+        offset_ms: 0,
+        uri: "spotify:track:2ofqFPNsZO7QY3GuEj9SW4"
     }
 ]
