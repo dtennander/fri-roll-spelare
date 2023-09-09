49e17d2f76d450591bdcf049848a25110d73f60d
Mårten Blankfors <marten@blankfors.se>
2023-09-09 08:24:08 +0200

    feat: Make color configurable (#5)

--- scripts/string_color.js
+++ scripts/string_color.js
@@ -1,12 +1,16 @@
+function songToColour(song) {
+  return song.colour ?? stringToColour(song.feeling);
+}
+
 function stringToColour(str) {
-    let hash = 0;
-    str.split('').forEach(char => {
-        hash = char.charCodeAt(0) + ((hash << 5) - hash)
-    })
-    let colour = '#'
-    for (let i = 0; i < 2; i++) {
-        const value = (hash >> (i * 8)) & 0xff
-        colour += "0" + value.toString(16).padStart(1, '0')
-    }
-    return colour + "ff"
+  let hash = 0;
+  str.split("").forEach((char) => {
+    hash = char.charCodeAt(0) + ((hash << 5) - hash);
+  });
+  let colour = "#";
+  for (let i = 0; i < 2; i++) {
+    const value = (hash >> (i * 8)) & 0xff;
+    colour += "0" + value.toString(16).padStart(1, "0");
+  }
+  return colour + "ff";
 }
