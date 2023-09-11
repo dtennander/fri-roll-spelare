84ac19cba5f64ae63c074dbf20fdf96c5a769bcc
Mårten Blankfors <marten@blankfors.se>
2023-09-11 10:43:02 +0200

    fix: Format `main.js`

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,28 @@
 function main() {
-    window.spotify.login()
-    for (const song of window.songs) {
-        document.getElementById("playlist").innerHTML +=
-            "<div style='background-color: " + songToColour(song) + "' class='button' id='" + song.feeling + "'> <img src='play.svg'>&emsp;" + song.feeling + "</div>";
-    }
+  window.spotify.login();
+  for (const song of window.songs) {
+    document.getElementById("playlist").innerHTML +=
+      "<div style='background-color: " + songToColour(song) +
+      "' class='button' id='" + song.feeling + "'> <img src='play.svg'>&emsp;" +
+      song.feeling + "</div>";
+  }
 
-    let slider = document.getElementById("slider");
-    window.spotify.setVolume(slider.value)
-    slider.onchange = function () { window.spotify.setVolume(slider.value) }
+  let slider = document.getElementById("slider");
+  window.spotify.setVolume(slider.value);
+  slider.onchange = function () {
+    window.spotify.setVolume(slider.value);
+  };
 
-    for (const song of window.songs) {
-        document.getElementById(song.feeling).onclick = function () { window.spotify.playSong(song.uri, song.offset_ms) }
-        document.getElementById(song.feeling).onpress = function () { window.spotify.playSong(song.uri, song.offset_ms) }
-    }
-    document.getElementById("togglePlay").onclick = window.spotify.pause
-    document.getElementById("togglePlay").onpress = window.spotify.pause
+  for (const song of window.songs) {
+    document.getElementById(song.feeling).onclick = function () {
+      window.spotify.playSong(song.uri, song.offset_ms);
+    };
+    document.getElementById(song.feeling).onpress = function () {
+      window.spotify.playSong(song.uri, song.offset_ms);
+    };
+  }
+
+  document.getElementById("togglePlay").onclick = window.spotify.pause;
+  document.getElementById("togglePlay").onpress = window.spotify.pause;
 }
-main()
+main();
